0ed90ec0814d863dac9ff685a94c1f87327818d6
Ashwin Date <coolbung@gmail.com>
2017-08-08 01:38:24 +0530

    Issue #44 feat:added router

--- code/site/router.php
+++ code/site/router.php
@@ -7,51 +7,74 @@
  * Work derived from the original RESTful API by Techjoomla (https://github.com/techjoomla/Joomla-REST-API) 
  * and the com_api extension by Brian Edgerton (http://www.edgewebworks.com)
 */
-
-function ApiBuildRoute( &$query )
+class APIRouter extends JComponentRouterBase
 {
-	$segments = array();
+	private  $views = array('documentation', 'keys');
 
-	if (isset($query['app'])) :
+
+	function build( &$query )
+	{
+		$segments = array();
+
+		if (isset($query['app'])) {
+			$segments[0] = $query['app'];
+			$segments[1] = $query['resource'];
+
+			if (isset($query['id'])) {
+				$segments[2] = $query['id'];			
+			}
+
+			unset($query['app'], $query['resource'], $query['id']);
+
+			return $segments;
+		}
+
+		if (isset($query['view'])) {
+			$segments[0] = $query['view'];
+			unset($query['view']);
+		}
+
+		if (isset($query['layout'])) {
+			$segments[1] = $query['layout'];
+			if ($query['layout'] == 'edit' && isset($query['id'])) :
+				$segments[2] = $query['id'];
+				unset($query['id']);
+			endif;
+			unset($query['layout']);
+		}
+
 		return $segments;
-	endif;
-
-	if (isset($query['view'])) {
-		$segments[0] = $query['view'];
-		unset($query['view']);
 	}
 
-	if (isset($query['layout'])) {
-		$segments[1] = $query['layout'];
-		if ($query['layout'] == 'edit' && isset($query['id'])) :
-			$segments[2] = $query['id'];
-			unset($query['id']);
-		endif;
-		unset($query['layout']);
+	/**
+	 * @param	array
+	 * @return	array
+	 */
+	function parse( &$segments )
+	{
+		$vars = array();
+
+		if (in_array($segments[0], $this->views)) {
+
+			$vars['view'] = $segments[0];
+			
+			if (isset($segments[1])) :
+				$vars['layout'] = $segments[1];
+			endif;
+			
+			if ($vars['layout'] == 'edit' && isset($segments[2])) :
+				$vars['id'] = $segments[2];
+			endif;			
+		} else {
+			$vars['format'] = 'raw';
+			$vars['app'] = $segments[0];
+			$vars['resource'] = $segments[1];
+
+			if (isset($segments[2])) {
+				$vars['id'] = $segments[2];
+			}
+		}
+		
+		return $vars;
 	}
-
-	return $segments;
-}
-
-/**
- * @param	array
- * @return	array
- */
-function ApiParseRoute( $segments )
-{
-	$vars = array();
-
-	if (isset($segments[0])) :
-		$vars['view'] = $segments[0];
-	endif;
-	
-	if (isset($segments[1])) :
-		$vars['layout'] = $segments[1];
-	endif;
-	
-	if ($vars['layout'] == 'edit' && isset($segments[2])) :
-		$vars['id'] = $segments[2];
-	endif;
-	
-	return $vars;
 }
