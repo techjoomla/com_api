6d6730203babd4971efba4541231fcda91ec1175
Ruud van Lent <info@onlinecommunityhub.nl>
2019-07-12 09:53:42 +0200

    Fix error message when method not found (or allowed)

--- code/site/libraries/resource.php
+++ code/site/libraries/resource.php
@@ -4,7 +4,7 @@
  * @copyright Copyright (C) 2009 2014 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
  * @license GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * @link http://techjoomla.com
- * Work derived from the original RESTful API by Techjoomla (https://github.com/techjoomla/Joomla-REST-API) 
+ * Work derived from the original RESTful API by Techjoomla (https://github.com/techjoomla/Joomla-REST-API)
  * and the com_api extension by Brian Edgerton (http://www.edgewebworks.com)
 */
 
@@ -30,7 +30,7 @@ abstract class ApiResource {
 			$this->$method_name();
 		else :
 
-			ApiError::raiseError(404, JText::_('COM_API_PLUGIN_METHOD_NOT_FOUND'));
+			ApiError::raiseError(404, JText::sprintf('COM_API_PLUGIN_METHOD_NOT_FOUND', $method_name));
 		endif;
 	}
 
