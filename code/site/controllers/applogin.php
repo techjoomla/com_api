d23b721cd23419b42e461f67d25a418c2a048a84
paritshivani <58217306+paritshivani@users.noreply.github.com>
2020-10-26 12:14:47 +0530

    Task#165153 fix: Create new view in TJLogin to post token to Ionic. (#124)

---
+++ code/site/controllers/applogin.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package    API
+ * @copyright  Copyright (C) 2009-2020 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
+ * @license    GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link       http://techjoomla.com
+ * Work derived from the original RESTful API by Techjoomla (https://github.com/techjoomla/Joomla-REST-API)
+ * and the com_api extension by Brian Edgerton (http://www.edgewebworks.com)
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Class for a Applogin Controller
+ *
+ * @since  2.5.2
+ */
+class ApiControllerApplogin extends ApiController
+{
+	/**
+	 * Typical view method for MVC based architecture
+	 *
+	 * This function is provide as a default implementation, in most cases
+	 * you will need to override it in your own controllers.
+	 *
+	 * @param   boolean  $cachable   If true, the view output will be cached
+	 * @param   array    $urlparams  An array of safe URL parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
+	 *
+	 * @return  \JControllerLegacy  A \JControllerLegacy object to support chaining.
+	 *
+	 * @since   3.0
+	 */
+	public function display($cachable = false, $urlparams = array())
+	{
+		parent::display();
+	}
+}
